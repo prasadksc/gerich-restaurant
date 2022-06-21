d1014700ec1895ebe6199852ccdbe3fa8dd1259b
prasadksc <prasadksc123@gmail.com>
2022-06-21 17:42:22 +0530

    first commit

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {images} from '../../constants'
+
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex-center'>
+      <img src={images.G} alt="g letter" />
+    </div>
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className='headtext__cormorant'>
+          About us
+        </h1>
+        <img src={images.spoon} alt="spoon img" />
+        <p className='p__opensans'>Sit tellus lobortis sed senectus vivamus molestie.<br /> Condimentum volutpat morbi facilisis quam scelerisque sapien.<br /> Et, penatibus aliquam amet tellus </p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+      <div className='app__aboutus-content_knife flex__center'>
+        <img src={images.knife} alt="knife img" />
+      </div>
+      <div className="app__aboutus-content_history">
+        <h1 className='headtext__cormorant'>
+          Our History
+        </h1>
+        <img src={images.spoon} alt="spoon img" />
+        <p className='p__opensans'>Sit tellus lobortis sed senectus vivamus molestie.<br /> Condimentum volutpat morbi facilisis quam scelerisque sapien.<br /> Et, penatibus aliquam amet tellus </p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+    </div>
+  </div>
+);
+
+export default AboutUs;
